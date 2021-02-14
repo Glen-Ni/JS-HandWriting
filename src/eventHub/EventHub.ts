bd6f9936303c81908bd3338ac22f8a488c8348a1
Nijialun <nijialun888@hotmail.com>
2021-02-14 23:10:05 +0800

    feat: 改进；添加测试用例

--- src/eventHub/EventHub.ts
+++ src/eventHub/EventHub.ts
@@ -12,8 +12,17 @@ class EventHub {
     // eventList.forEach(fn => {
     //   fn.call(this, ...props);
     // });
-    (this.cache[eventName] || []).forEach(fn => {fn.call(this, ...props)});
+    (this.cache[eventName] || []).forEach(fn => {fn(...props)});
   }
+  off = (eventName, fn) => {
+    // 我的写法，会过滤掉一个数组中所有fn
+    this.cache[eventName] = (this.cache[eventName] || []).filter(
+      (item) => item !== fn
+    );
+    // 别人的写法，用indexof 删除第一项
+    // 数组的indexof在ie12才可以用，最好自己写个indexOf方法
+  }
+
 }
 
 export default EventHub;
--- src/eventHub/test.ts
+++ src/eventHub/test.ts
@@ -2,12 +2,37 @@ import EventHub from './EventHub';
 
 const eventHub = new EventHub();
 
-console.assert(eventHub instanceof Object === true, 'eventHub 是个对象')
+const test1 = (message) => {
+  console.assert(eventHub instanceof Object === true, 'eventHub 是个对象');
+  console.log(message);
+}
 
-let called = false;
-eventHub.on('test', y => {
-  called = true;
-  console.log('called: ' + called);
-  console.assert(y === '你是傻逼');
-})
-eventHub.emit('test', '你是傻逼');
+const test2 = (message) => {
+  let called = false;
+  eventHub.on('test', (a,b) => {
+    called = true;
+    console.assert(a === '我我我');
+    console.assert(b === '那叫一个帅');
+  });
+  eventHub.emit('test', '我我我', '那叫一个帅');
+  console.assert(called);
+  console.log(message);
+};
+
+const test3 = (message) => {
+  let called = false;
+  const fn = y => {
+    called = true;
+  };
+  eventHub.on('test2', fn);
+  eventHub.off('test2', fn);
+  eventHub.emit('test2');
+  setTimeout(() => {
+    console.assert(!called);
+  }, 1000);
+  console.log(message);
+};
+
+test1('1. EventHub 可创建对象');
+test2('2. .on后.emit，可触发函数');
+test3('3. .off有用');
