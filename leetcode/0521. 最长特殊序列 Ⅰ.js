886004a7d48df4fd7bad8c41957404b91808520f
小僵尸RUA <nijialun888@hotmail.com>
2022-03-05 08:44:48 +0800

    0521

---
+++ leetcode/0521. 最长特殊序列 Ⅰ.js
@@ -0,0 +1,14 @@
+/**
+ * 无聊
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var findLUSlength = function (a, b) {
+  if (a === b) {
+    return -1;
+  }
+  return Math.max(a.length, b.length);
+};
+
+console.log(findLUSlength("abc", "bca"));
